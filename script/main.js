5638fb188f2bfcb5a25ea111c1904f5435aa3d9f
madoka <mnogaki@my.bcit.ca>
2025-03-09 08:28:13 -0700

    adjust styling on main.html

--- script/main.js
+++ script/main.js
@@ -23,14 +23,12 @@ let point = 0;
 let currIngred = [];
 let currOrder = avaDrinks[Math.floor(Math.random() * avaDrinks.length)];
 let doneOrder = [];
-// let doneOrder = ["Thai Basil Sangria"];
 
 document.addEventListener("load", setup());
 
 function setup() {
-    countdownTimer(0, 60);
+    countdownTimer(1, 0);
     print();
-    // document.getElementById("score").innerHTML = point;
 }
 
 function countdownTimer(minutes, seconds) {
@@ -41,52 +39,35 @@ function countdownTimer(minutes, seconds) {
             //window.onload = function() {window.document.body.onload = setupEnd};
         }
         var counter = document.getElementById("timer");
-        counter.innerHTML = String(minutes) + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        counter.innerHTML =
+            minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         seconds--;
         if (seconds >= 0) {
             setTimeout(tick, 1000);
-            clearTimeout();
         } else {
             if (minutes >= 1) {
                 setTimeout(function () {
                     countdownTimer(minutes - 1, 59);
                 }, 1000);
-                clearTimeout();
             }
+            // end game
+            // window.location.href = "";
         }
     }
     tick();
 }
 
-// window.load = function() {
-//     window.document.body.onload = setupEnd();
-// }
-
 function print() {
-    displayCustomer();
-    var customer = document.getElementById("customerOrder");
-    customerOrder.innerHTML = "<p>" + currOrder.name + " please <p>";
+    // var customer = document.getElementById("customer");
+    // customer.innerHTML = currOrder.name + " please";
     var instructions = document.getElementById("instructions");
-    instructions.innerHTML = "<p> Ingredients: <p> " + currOrder.ingred.join("<p>") + "<p> Action: " + currOrder.action;
-}
-
-function displayCustomer() {
-    var customersArr = ["assets/character_01.svg", "assets/character_02.svg", "assets/character_03.svg"];
-    var randomNum = Math.floor(Math.random() * 3);
-    document.customer.src = customersArr[randomNum];
+    instructions.innerHTML = "Order: " + currOrder.name + "<p> Ingredients: <p> " +
+    currOrder.ingred.join("<p>") + "<p> Action: " + currOrder.action;
 }
 
 function addIngred(ingredient) {
     currIngred.push(ingredient);
 }
-
-document.querySelectorAll(".under-counter-container img").forEach(img => {
-    const defaultSrc = img.src;
-    const hoverSrc = defaultSrc.replace("buttons_default", "buttons_hover");
-
-    img.addEventListener("mouseover", () => img.src = hoverSrc);
-    img.addEventListener("mouseout", () => img.src = defaultSrc);
-});
 
 function correctIngred() {
     let correct = (currOrder.ingred.length == currIngred.length);
@@ -100,15 +81,11 @@ function correctIngred() {
     return correct;
 }
 
-
 function doOrder(action) {
-    if (action == "Stir")
-        element = document.getElementById("stirring");
-    else
-        element = document.getElementById("shaking");
+    element = document.getElementById("animation");
     element.style.visibility = "visible";
     setTimeout(function() {
-        element.style.visibility = "hidden";
+        document.getElementById("test").style.visibility = "hidden";
         if (correctIngred() && action == currOrder.action) {
             doneOrder.push(currOrder.name);
             updateScore();
@@ -118,36 +95,6 @@ function doOrder(action) {
         print();
     }, 1000);
 }
-
-// function doOrder(action) {
-//     if (action == "Stir")
-//         element = document.getElementById("stirring");
-//     else
-//         element = document.getElementById("shaking");
-//     element.style.visibility = "visible";
-//     setTimeout(function() {
-//         element.style.visibility = "hidden";
-//         drink = document.getElementById(currOrder.name);
-//         drink.style.visibility = "visible";
-//         setTimeout(function() {
-//             drink.style.visibility = "hidden";
-//             if (correctIngred() && action == currOrder.action) {
-//                 doneOrder.push(currOrder.name);
-//                 updateScore();
-//             }
-//             currIngred = [];
-//             currOrder = avaDrinks[Math.floor(Math.random() * avaDrinks.length)];
-//             print();
-//         }, 800);
-//     }, 1000);
-// }
-
-// completes = document.querySelectorAll(".completes");
-// completes.forEach(complete => {
-//     if (complete.getAttributes("data-info") == "") {
-        
-//     }
-// });
 
 function updateScore() {
     var score = document.getElementById("score");
