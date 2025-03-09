751e10a622c06fe0dc17af6b60504202f97db4bc
Desiree Ng <desireeng329@gmail.com>
2025-03-09 06:52:28 -0700

    implemented functions for main.js

--- script/main.js
+++ script/main.js
@@ -19,46 +19,76 @@ let point = 0;
 let currIngred = [];
 let currOrder = avaDrinks[Math.floor(Math.random() * avaDrinks.length)];
 let doneOrder = [];
+// let doneOrder = ["Thai Basil Sangria"];
 
 document.addEventListener("load", setup());
 
 function setup() {
-    countdownTimer(1, 0);
+    countdownTimer(0, 30);
     print();
+    // document.getElementById("score").innerHTML = point;
 }
 
 function countdownTimer(minutes, seconds) {
     function tick() {
+        if (seconds == 0 && minutes == 0) {
+            clearTimeout();
+            window.location.href = "end.html";
+            document.getElementById("end").addEventListener("load", setupEnd());
+    //         window.onload = function() {
+    //             document.getElementById("finalscore").innerHTML = point;
+    // document.getElementById("martini").innerHTML = frequency("Green Apple Martini");
+    // document.getElementById("margarita").innerHTML = frequency("Classic Margarita");
+    // document.getElementById("thai").innerHTML = frequency("Thai Basil Sangria");
+    //         }        
+        }  
         var counter = document.getElementById("timer");
-        counter.innerHTML =
-            minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        counter.innerHTML = String(minutes) + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         seconds--;
         if (seconds >= 0) {
             setTimeout(tick, 1000);
+            clearTimeout();
         } else {
             if (minutes >= 1) {
                 setTimeout(function () {
                     countdownTimer(minutes - 1, 59);
                 }, 1000);
+                clearTimeout();
             }
-            // end game
-            // window.location.href = "";
         }
     }
     tick();
 }
 
+// window.load = function() {
+//     window.document.body.onload = setupEnd();
+// }
+
 function print() {
-    // var customer = document.getElementById("customer");
-    // customer.innerHTML = currOrder.name + " please";
+    displayCustomer();
+    var customer = document.getElementById("customerOrder");
+    customerOrder.innerHTML = "<p>" + currOrder.name + " please <p>";
     var instructions = document.getElementById("instructions");
-    instructions.innerHTML = "Order: " + currOrder.name + "<p> Ingredients: <p> " +
-    currOrder.ingred.join("<p>") + "<p> Action: " + currOrder.action;
+    instructions.innerHTML = "<p> Ingredients: <p> " + currOrder.ingred.join("<p>") + "<p> Action: " + currOrder.action;
+}
+
+function displayCustomer() {
+    var customersArr = ["assets/character_01.svg", "assets/character_02.svg", "assets/character_03.svg"];
+    var randomNum = Math.floor(Math.random() * 3);
+    document.customer.src = customersArr[randomNum];
 }
 
 function addIngred(ingredient) {
     currIngred.push(ingredient);
 }
+
+document.querySelectorAll(".under-counter-container img").forEach(img => {
+    const defaultSrc = img.src;
+    const hoverSrc = defaultSrc.replace("buttons_default", "buttons_hover");
+
+    img.addEventListener("mouseover", () => img.src = hoverSrc);
+    img.addEventListener("mouseout", () => img.src = defaultSrc);
+});
 
 function correctIngred() {
     let correct = (currOrder.ingred.length == currIngred.length);
@@ -72,11 +102,15 @@ function correctIngred() {
     return correct;
 }
 
+
 function doOrder(action) {
-    element = document.getElementById("animation");
+    if (action == "Stir")
+        element = document.getElementById("stirring");
+    else
+        element = document.getElementById("shaking");
     element.style.visibility = "visible";
     setTimeout(function() {
-        document.getElementById("test").style.visibility = "hidden";
+        element.style.visibility = "hidden";
         if (correctIngred() && action == currOrder.action) {
             doneOrder.push(currOrder.name);
             updateScore();
@@ -87,16 +121,55 @@ function doOrder(action) {
     }, 1000);
 }
 
+// function doOrder(action) {
+//     if (action == "Stir")
+//         element = document.getElementById("stirring");
+//     else
+//         element = document.getElementById("shaking");
+//     element.style.visibility = "visible";
+//     setTimeout(function() {
+//         element.style.visibility = "hidden";
+//         drink = document.getElementById(currOrder.name);
+//         drink.style.visibility = "visible";
+//         setTimeout(function() {
+//             drink.style.visibility = "hidden";
+//             if (correctIngred() && action == currOrder.action) {
+//                 doneOrder.push(currOrder.name);
+//                 updateScore();
+//             }
+//             currIngred = [];
+//             currOrder = avaDrinks[Math.floor(Math.random() * avaDrinks.length)];
+//             print();
+//         }, 800);
+//     }, 1000);
+// }
+
+// completes = document.querySelectorAll(".completes");
+// completes.forEach(complete => {
+//     if (complete.getAttributes("data-info") == "") {
+        
+//     }
+// });
+
 function updateScore() {
     var score = document.getElementById("score");
     score.innerHTML = String(++point);
 }
 
-function frequency(order) {
-    let i = 0;
-    doneOrder.forEach(done => {
-        if (done == order)
-            i++;
-    });
-    return i;
-}
+// document.getElementById("end").addEventListener("load", setupEnd());
+// function setupEnd() {
+//     document.getElementById("finalscore").innerHTML = 87;
+//     document.getElementById("martini").innerHTML = frequency("Green Apple Martini");
+//     document.getElementById("margarita").innerHTML = frequency("Classic Margarita");
+//     document.getElementById("thai").innerHTML = frequency("Thai Basil Sangria");
+// }
+
+// function frequency(order) {
+//     let doneOrder = JSON.parse(localStorage.getItem("doneOrder")) || [];
+//     let i = 0;
+//     doneOrder.forEach(done => {
+//         if (done == order)
+//             i++;
+//     });
+//     return i;
+// }
